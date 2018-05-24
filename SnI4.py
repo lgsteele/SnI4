d99315a402cb4d5a17681de6ce27acb429117af8
Louis Steele <louis.g.steele@gmail.com>
2018-05-23 23:59:34 -0700

    Added to T1 and T2 calculations/graphs.

--- SnI4.py
+++ SnI4.py
@@ -19,25 +19,25 @@ zeeman,zeeman_signal =\
            delimiter=',',skiprows=1,unpack=True)
 
 # fsweep fit (Gaussian)
-def gauss_fit(freq,A,f0,y0,width):
-    gauss = y0 + A*np.exp(-((freq-f0)**2)/(2*(width**2)))
-    return gauss
-p0 = np.array([16,409.3,0,.005])
-coeffs, matcov = curve_fit(gauss_fit,fsweep,fsweep_signal,p0)
-fsweep_fit = gauss_fit(fsweep,coeffs[0],\
-                       coeffs[1], coeffs[2],\
-                       coeffs[3])
-print coeffs
-try:    
-    plt.plot(fsweep,fsweep_signal,'x',fsweep,fsweep_fit,'k-')
-    plt.title('Zero-field NQR Resonance of tin(IV) iodide')
-    plt.xlabel('RF Frequency (MHz)')
-    plt.ylabel('Signal (AU)')
-    plt.text(409.35,10,'f0 = %s MHz\n\nwidth = %s MHz'%\
-             (round(coeffs[1],3),round(coeffs[3],4)))
-    plt.show()
-except KeyboardInterrupt:
-    plt.close()
+##def gauss_fit(freq,A,f0,y0,width):
+##    gauss = y0 + A*np.exp(-((freq-f0)**2)/(2*(width**2)))
+##    return gauss
+##p0 = np.array([16,409.3,0,.005])
+##coeffs, matcov = curve_fit(gauss_fit,fsweep,fsweep_signal,p0)
+##fsweep_fit = gauss_fit(fsweep,coeffs[0],\
+##                       coeffs[1], coeffs[2],\
+##                       coeffs[3])
+##print coeffs
+##try:    
+##    plt.plot(fsweep,fsweep_signal,'x',fsweep,fsweep_fit,'k-')
+##    plt.title('Zero-field NQR Resonance of tin(IV) iodide')
+##    plt.xlabel('RF Frequency (MHz)')
+##    plt.ylabel('Signal (AU)')
+##    plt.text(409.35,10,'f0 = %s MHz\n\nwidth = %s MHz'%\
+##             (round(coeffs[1],3),round(coeffs[3],4)))
+##    plt.show()
+##except KeyboardInterrupt:
+##    plt.close()
 
 
 # t90
@@ -66,13 +66,14 @@ except KeyboardInterrupt:
 ##print coeffs
 ##try:    
 ##    plt.semilogx(T1,T1_signal,'x',label="Inversion recovery sequence data")
-####    plt.semilogx(T1,T1_test_fit,'g')
-##    plt.semilogx(T1,T1_fit,'k',label="T1 fit")
-##    plt.title('Inversion recovery (T1) of tin(IV) iodide')
-##    plt.xlabel('t_wait (us)')
+##    plt.semilogx(T1,T1_fit,'k',label=r"$T_1$ fit")
+##    plt.title('Inversion recovery of $SnI_4$ 3/2<->5/2 resonance')
+##    plt.xlabel(r'$t_{wait}$ (s)')
 ##    plt.ylabel('Signal (AU)')
-##    plt.text(1e-3,6.5,'a = 0.586, T1a = 185us')
-##    plt.text(1e-3,6,'b = 2.121, T1b = 127us')
+##    plt.text(1e-4,6.5,\
+##    r'$M_z(t_{wait}) = M_0\left[1-\alpha\left(ae^{-t_{wait}/T_{1a}}+be^{-t_{wait}/T_{1b}}\right)\right]$')
+##    plt.text(1e-4,6,r'a = 0.586, $T_{1a} = 185\mu s$')
+##    plt.text(1e-4,5.5,r'b = 2.121, $T_{1b} = 127\mu s$')
 ##    plt.legend(loc='center right')
 ##    plt.show()
 ##except KeyboardInterrupt:
@@ -80,6 +81,27 @@ except KeyboardInterrupt:
 
 
 
+# T2
+##def T2_fit(tau,Mxy,T2,y0):
+##    T2_func = Mxy*np.exp(-2*tau/T2)+y0
+##    return T2_func
+##T2_test_fit = T2_fit(T2,14,.00015,.3)
+##p0 = np.array([14,.00015,.3])
+##coeffs, matcov = curve_fit(T2_fit,T2,T2_signal,p0)
+##print coeffs
+##T2_fit = T2_fit(T2,coeffs[0],coeffs[1],coeffs[2])
+##try:
+##    plt.plot(T2*10**6,T2_signal,'xb',label="Hahn echo sequence data")
+##    plt.plot(T2*10**6,T2_fit,'k',label=r'$T_2$ Fit')
+##    plt.legend(loc='upper right')
+##    plt.title(r'Hahn echo data and $T_2$ fitting for $SnI_4$ 3/2<->5/2 resonance')
+##    plt.xlabel(r'$\tau$ (us)')
+##    plt.ylabel('Hahn echo signal')
+##    plt.text(150,5,r'$T_2 = 107\mu s$')
+##    plt.text(150,6,r'$M_{xy}(t) = M_{xy}(0)e^{-2\tau/T_2}$')
+##    plt.show()
+##except KeyboardInterrupt:
+##    plt.close()
 
 
 
